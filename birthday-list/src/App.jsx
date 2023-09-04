8fde6957297abb000c78db95f6b957abba5a43f2
Perawich Sirisopon <perawichsirisopoon@gmail.com>
2023-09-04 10:46:41 +0700

    update

--- birthday-list/src/App.jsx
+++ birthday-list/src/App.jsx
@@ -5,14 +5,17 @@ import data from './data'
 
 function App() {
   const [people, setPeople] = useState(data)
+  const clearAll = () =>{
+    setPeople([])
+  }
   return (
     <div>
       <h1>Workshop1: Birthday List</h1>
       <main>
         <section className='container'>
-          <h3>5 Birthdays today</h3>
+          <h3>{people.length} Birthdays today</h3>
           <List people = {people} />
-          <button>Clear All</button>
+          <button onClick={clearAll}>Clear All</button>
         </section>
       </main>
     </div>
