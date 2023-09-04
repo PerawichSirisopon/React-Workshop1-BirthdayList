27d770fe9be44fa9243020dfe5e84d54ccf86812
Perawich Sirisopon <perawichsirisopoon@gmail.com>
2023-09-04 10:28:45 +0700

    Hello

---
+++ birthday-list/src/data.js
@@ -0,0 +1,33 @@
+const data = [{
+        id: 1,
+        name: "Bertie Yates",
+        age: "29",
+        image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v159559959131/person-2_ipcjws.jpg"
+    },
+    {
+        id: 2,
+        name: "Hester Hogan",
+        age: "32",
+        image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+    },
+    {
+        id: 3,
+        name: "Larry Little",
+        age: "36",
+        image: "https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+    },
+    {
+        id: 4,
+        name: "Sean Walsh",
+        age: "34",
+        image: "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1587&q=80"
+    },
+    {
+        id: 5,
+        name: "Lola Gardner",
+        age: "29",
+        image: "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1522&q=80"
+    },
+];
+
+export default data;
